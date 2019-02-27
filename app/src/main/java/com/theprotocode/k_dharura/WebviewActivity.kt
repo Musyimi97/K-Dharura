a527ec14236b39e3c5079b4412c2e5ff39fcd47e
Labohkip81 <Labankiplagat81@gmail.com>
2019-02-27 22:17:27 +0300

    Redesigned the home page of the application

---
+++ app/src/main/java/com/theprotocode/k_dharura/WebviewActivity.kt
@@ -0,0 +1,12 @@
+package com.theprotocode.k_dharura
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WebviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_webview)
+    }
+}
