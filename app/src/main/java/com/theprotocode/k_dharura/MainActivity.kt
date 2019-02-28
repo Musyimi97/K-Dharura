6d1991861754a425350e5dce120b4a92b3bab488
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-03-01 00:23:28 +0300

    New commit for some icons and a lil bit of backend

---
+++ app/src/main/java/com/theprotocode/k_dharura/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.theprotocode.k_dharura
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+//    Am now going to get reference to all my image views in my homepage
+// and set onClickListener and intents to listen to onClicks
+
+
+////This intent opens phone keypad automatically
+
+    fun EmergencyNumber(view: View){
+        val intent = Intent(Intent.ACTION_DIAL)
+        intent.data = Uri.parse("tel:999")
+        startActivity(intent)
+    }
+
+
+////This code opens camera automatically and sends it to email automatically.
+    val REQUEST_IMAGE_CAPTURE = 1
+    fun dispatchTakePictureIntent(view: View) {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+
+            }
+        }
+    }
+
+
+
+}
+
+
+
+
+
