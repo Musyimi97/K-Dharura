93a66f0c268def6ab089c132b0a4aa14980fcba4
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-03-02 02:26:02 +0300

    photo mail

--- app/src/main/java/com/theprotocode/k_dharura/MainActivity.kt
+++ app/src/main/java/com/theprotocode/k_dharura/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.theprotocode.k_dharura
 
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.support.v4.content.ContextCompat
 import android.support.v4.content.ContextCompat.startActivity
+import android.support.v4.content.FileProvider
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
-
+import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,11 +22,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        first aid kit image button onclick
+
         firstAidKit.setOnClickListener {
             val intent = Intent(this, ScrollingActivity::class.java)
             // start your next activity
             startActivity(intent)
         }
+
+//         Photo mail onclick  listener
+
 
     }
 
@@ -31,42 +39,50 @@ class MainActivity : AppCompatActivity() {
     //This intent opens phone keypad automatically
 
     fun callNumber(view: View){
-        val intent = Intent(Intent.ACTION_CALL)
+        val intent = Intent(Intent.ACTION_DIAL)
         intent.data = Uri.parse("tel:999")
         startActivity(intent)
     }
+
+
+//This code opens camera automatically, takes a picture and sends it to the email
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    fun takePicture(view: View) {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+
+            }
+        }
+    }
+//       working on a code to take image and send it to a contact via email
+//
+//    // getExternalFilesDir() + "/Pictures" should match the declaration in fileprovider.xml paths
+//    val file = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "share_image_" + System.currentTimeMillis() + ".png")
+//
+//    // wrap File object into a content provider. NOTE: authority here should match authority in manifest declaration
+//    val bmpUri = FileProvider.getUriForFile(MainActivity.this, "com.theprotocode.k-dharura", file)
+//
+//    val intent = Intent().apply {
+//        this.action = Intent.ACTION_SEND
+//        this.putExtra(Intent.EXTRA_STREAM, bmpUri)
+//        this.type = "image/jpeg"
+//    }
+//    startActivity(Intent.createChooser(intent, resources.getText(R.string.se
+//
+
+
+
+
 
 
 
 }
 
 
-//    Am now going to get reference to all my image views in my homepage
-// and set onClickListener and intents to listen to onClicks
 
 
-//    fun breakdownPageActivity(view: View){
-//        val intent=Intent(this,breakdownPageActivity::class.java)
-//        startActivity(intent)
-//    }
-
-
-
-////This code opens camera automatically and sends it to email automatically.
-//    val REQUEST_IMAGE_CAPTURE = 1
-//
-//    fun dispatchTakePictureIntent(view: View) {
-//
-//
-//
-//
-//        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-//            takePictureIntent.resolveActivity(packageManager)?.also {
-//                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-//
-//            }
-//        }
-//    }
 
 
 
