be9bd00f7360372fac6c4509ca3dc32e38a481aa
Labohkip81 <Labankiplagat81@gmail.com>
2019-03-02 12:04:22 +0300

    Added first aid tips

--- app/src/main/java/com/theprotocode/k_dharura/MainActivity.kt
+++ app/src/main/java/com/theprotocode/k_dharura/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.theprotocode.k_dharura
 
+import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.support.v4.content.ContextCompat.startActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
-
-
+import kotlinx.android.synthetic.main.activity_main.*
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.support.v4.app.ActivityCompat.startActivityForResult
+import android.support.v4.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,14 +22,72 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        details.setOnClickListener {
+
+
+
+
+
+
+
+        class pol(view: View){
+            fun call(view: View) {
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:999")
+                startActivity(intent)
+            }
+
+        }
+
+
+
+        firstAidTips.setOnClickListener {
             val intent = Intent(this, ScrollingActivity::class.java)
             // start your next activity
             startActivity(intent)
+
         }
+
+
+////This intent opens phone keypad automatically
+
+        police.setOnClickListener{
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:999")
+            startActivity(intent)
+         }
+
+        fun callNumber(view: View){
+            val intent =Intent(Intent.ACTION_SCREEN_OFF)
+            startActivity(intent)
+        }
+
+        fun call(view: View) {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:999")
+            startActivity(intent)
+        }
+
+
+        ////This code opens camera automatically and sends it to email automatically.
+        val REQUEST_IMAGE_CAPTURE = 1
+        fun dispatchTakePictureIntent(view: View) {
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                takePictureIntent.resolveActivity(packageManager)?.also {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+
+                }
+            }
+        }
+
 
     }
 }
+
+//class MainActivity : AppCompatActivity() {
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
 
 
 //    Am now going to get reference to all my image views in my homepage
@@ -36,12 +99,14 @@ class MainActivity : AppCompatActivity() {
 //        startActivity(intent)
 //    }
 //
+
+
 ////This intent opens phone keypad automatically
-//
+
 //    fun EmergencyNumber(view: View){
 //        val intent = Intent(Intent.ACTION_DIAL)
 //        intent.data = Uri.parse("tel:999")
-//        startActivity(intent)
+//        startActivity(Context)
 //    }
 
 
@@ -60,8 +125,8 @@ class MainActivity : AppCompatActivity() {
 //            }
 //        }
 //    }
-
-
+//
+//
 
 
 
--- app/src/main/java/com/theprotocode/k_dharura/ScrollingActivity.kt
+++ app/src/main/java/com/theprotocode/k_dharura/ScrollingActivity.kt
@@ -7,6 +7,9 @@ import kotlinx.android.synthetic.main.activity_scrolling.*
 
 class ScrollingActivity : AppCompatActivity() {
 
+
+
+//This activity will contain procedures for first aid incase of an emergency
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scrolling)
@@ -17,3 +20,86 @@ class ScrollingActivity : AppCompatActivity() {
         }
     }
 }
+
+
+//package com.theprotocode.k_dharura
+//
+//import android.content.Intent
+//import android.graphics.Bitmap
+//import android.net.Uri
+//import android.support.v7.app.AppCompatActivity
+//import android.os.Bundle
+//import android.provider.MediaStore
+//import android.view.View
+//
+//class MainActivity : AppCompatActivity() {
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
+////    Am now going to get reference to all my image views in my homepage
+//// and set onClickListener and intents to listen to onClicks
+//
+//
+//////This intent opens phone keypad automatically
+//
+//    fun EmergencyNumber(view: View){
+//        val intent = Intent(Intent.ACTION_DIAL)
+//        intent.data = Uri.parse("tel:999")
+//        startActivity(intent)
+//    }
+//
+//
+//    ////This code opens camera automatically and sends it to email automatically.
+//    val REQUEST_IMAGE_CAPTURE = 1
+//    fun dispatchTakePictureIntent(view: View) {
+//        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+//            takePictureIntent.resolveActivity(packageManager)?.also {
+//                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+//
+//            }
+//        }
+//    }
+//
+//
+//
+//}
+
+
+
+
+//// when button is clicked, show the alert
+//editHealthId.setOnClickListener {
+//    // build alert dialog
+//    val dialogBuilder = AlertDialog.Builder(this)
+//
+//    // set message of alert dialog
+//    dialogBuilder.setMessage("Do you want to close this application ?")
+//        // if the dialog is cancelable
+//        .setCancelable(false)
+//        // positive button text and action
+//        .setPositiveButton("Proceed", DialogInterface.OnClickListener {
+//                dialog, id -> finish()
+//        })
+//        // negative button text and action
+//        .setNegativeButton("Cancel", DialogInterface.OnClickListener {
+//                dialog, id -> dialog.cancel()
+//        })
+//
+//    // create dialog box
+//    val alert = dialogBuilder.create()
+//    // set title for alert dialog box
+//    alert.setTitle("AlertDialogExample")
+//    // show alert dialog
+//    alert.show()
+//}
+
+
+//
+//editHealthId.setOnClickListener {
+//    val intent = Intent(this, ScrollingActivity::class.java)
+//    // start your next activity
+//    startActivity(intent)
+//
+//}
